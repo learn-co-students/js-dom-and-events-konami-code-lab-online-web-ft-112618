089a48019086fe3dfa357acfcec4916f1dd5f810
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-03-18 10:49:52 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let i = 0
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  const main = document.body
+  main.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log(e.detail || e.which)
+    if (key === code[i]) {
+        i++
+        if (i === code.length) {
+          alert('Yay, code correct!')
+          i = 0
+        }
+    } else {
+      i = 0
+    }
+  })
 }
